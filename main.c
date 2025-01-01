c88887ada43779f3579f0b7513fb20f9eaed85ce
swalihkzr1111 <mswalih0999@gmail.com>
2025-01-01 13:03:00 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+division of two numbers
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{   int num1,num2,result;
+    printf("enter two numbers");
+    scanf("%d%d",&num1,&num2);
+    result=num1/num2;
+    printf("result is %d",result);
+
+    return 0;
+}
